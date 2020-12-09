d4b6643b114b0ceea79cc7929e55ff8e4eadbe06
peace_love <1991246280@qq.com>
2020-12-09 08:20:22 +0800

    firstcommit

---
+++ itheimagittest02/src/main/java/pojo/User.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class User {
+    public String name;
+    public String emile;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmile() {
+        return emile;
+    }
+
+    public void setEmile(String emile) {
+        this.emile = emile;
+    }
+}
